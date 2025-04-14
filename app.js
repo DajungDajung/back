4a1976e009f5e17b11f14c007feec8c40cd6635f
jiseong1688 <wltjd1688@naver.com>
2025-04-14 15:43:15 +0900

    build: nodemon추가 설치 및 .env설정

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require('express');
+const express =require('express');
 const app = express();
+
 const dotenv = require('dotenv');
 dotenv.config();
 
+app.use(express.json());
+
 app.listen(process.env.PORT);
+
+const itemRouter = require('./routes/items');
+
+app.use('/items', itemRouter);
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+})
