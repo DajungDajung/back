1a7d7cd4a9d4b51518468e9e7cd5ffca17e2bfd3
JungEun <foreverkiddd@gmail.com>
2025-04-26 13:11:27 +0900

    fix: 좋아요 조회 및 댓글 조회 수정 좋아요 조회 및 댓글 조회 SQL문 수정

--- src/controller/CommentController.js
+++ src/controller/CommentController.js
@@ -1,76 +1,60 @@
 // const ensureAuthorization = require('../auth');
 // const jwt = require('jsonwebtoken');
-const conn = require('../mariadb');
-const {StatusCodes} = require('http-status-codes');
+const conn = require("../mariadb");
+const { StatusCodes } = require("http-status-codes");
 
 const addComment = (req, res) => {
-    const item_id = req.params.id;
-    const {user_id, contents} = req.body;
+  const item_id = req.params.id;
+  const { user_id, contents } = req.body;
 
-    let sql = `INSERT INTO comments (item_id, user_id, contents)
+  let sql = `INSERT INTO comments (item_id, user_id, contents)
                 VALUES (?, ?, ?)`;
-    let values = [item_id, user_id, contents];
-    conn.query(sql, values,
-        (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
+  let values = [item_id, user_id, contents];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
 
-            return res.status(StatusCodes.OK).json(results);
-        }
-    )
+    return res.status(StatusCodes.OK).json(results);
+  });
 };
 
 const removeComment = (req, res) => {
-    const commentId = req.params.id;
-    const {user_id} = req.body;
+  const commentId = req.params.id;
+  const { user_id } = req.body;
 
-    let sql = `DELETE FROM comments
+  let sql = `DELETE FROM comments
                 WHERE id = ? AND user_id = ?`;
-    let values = [commentId, user_id];
-    conn.query(sql, values,
-        (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
+  let values = [commentId, user_id];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
 
-            return res.status(StatusCodes.OK).json(results);
-        }
-    )
-    
+    return res.status(StatusCodes.OK).json(results);
+  });
 };
 
 const commentList = (req, res) => {
-    const item_id = req.params.id;
+  const item_id = req.params.id;
 
-    let sql = `SELECT * FROM comments
+  let sql = `SELECT img_id, nickname, contents FROM comments
+                LEFT JOIN users ON comments.user_id = users.id
                 WHERE item_id = ?`;
-    conn.query(sql, item_id,
-        (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-            if(results.length) {
-                results.map(function(result) {
-                    result.itemId = result.item_id;
-                    delete result.item_id;
-                    result.userId = result.user_id;
-                    delete result.user_id;
-                });
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
-        }
-    )
-    
+  conn.query(sql, item_id, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    return res.status(StatusCodes.OK).json(results);
+  });
 };
 
 module.exports = {
-    addComment,
-    removeComment,
-    commentList
+  addComment,
+  removeComment,
+  commentList,
 };
