b32de4edd2516f6ffa3fcae32c9911ca2433991e
jiseong1688 <wltjd1688@naver.com>
2025-05-07 16:40:10 +0900

    fix: 상품상세가 로그인 유무에 따라 보일 수 있도록 해결

--- src/routes/items.js
+++ src/routes/items.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { getItems, getItemDetail, postItem, updateItem, deleteItem, getMyItems, getLikedItems } = require('../controller/ItemController.js');
 const checkAuthorization = require('../modules/auth/checkAuthorization.js');
 const checkItemOwner = require('../modules/auth/checkItemOwner.js');
+const passAuthorization = require('../modules/auth/passAuthorization.js');
 
 // 상품 전체 조회 및 상품 검색 && 상품 등록
 router.route('/')
@@ -14,7 +15,7 @@ router.route('/myitem')
 
 // 상품 상세 정보 조회 && 상품 수정 && 상품 삭제
 router.route(`/:id(\\d+)`)
-    .get(checkAuthorization, getItemDetail)
+    .get(passAuthorization, getItemDetail)
     .put(checkAuthorization, checkItemOwner, updateItem)
     .delete(checkAuthorization, checkItemOwner, deleteItem);
 
