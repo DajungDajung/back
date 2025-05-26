75592cfb95578510dbc0237f05de139f50b9f490
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2025-05-26 16:36:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin: any, callback: any) {
+    origin: function (origin, callback) {
       if (!origin) return callback(null, false);
       if (allowedOrigins.includes(origin)) {
         return callback(null, origin);
