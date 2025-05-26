159dd4056340ff43d95c7f5e14ad49b0bb48eed5
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2025-05-26 17:23:22 +0900

    fix : 오류를 발견하여 잠시 js로 롤백합니다

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,15 @@
-import express from "express";
+const express = require("express");
+const router = express.Router();
 
-import {
+const {
   signUp,
   signIn,
   findId,
   passwordResetRequest,
   passwordReset,
   logout,
-} from "../controller/AuthController";
+} = require("../controller/AuthController");
 const checkAuthorization = require("../modules/auth/checkAuthorization");
-
-const router = express.Router();
 
 router.use(express.json());
 
@@ -22,4 +21,7 @@ router.put("/reset", passwordReset);
 router.delete("/logout", checkAuthorization, logout);
 
 module.exports = router;
+<<<<<<< HEAD:src/routes/auth.ts
 // export default router;
+=======
+>>>>>>> b08e083 (fix : 오류를 발견하여 잠시 js로 롤백합니다):src/routes/auth.js
